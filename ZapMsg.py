9a1dead1128acc6d20617ceb5265c08f7d5f37ee
Kaio Gato <kaiomurilo017@gmail.com>
2024-05-28 07:41:10 -0400

    28/05/2024

---
+++ ZapMsg.py
@@ -0,0 +1,6 @@
+    def count(a):
+    x=a
+    while(x<=10):
+        print(x)
+        x=x+1
+        
---
+++ media.py
@@ -0,0 +1,4 @@
+# Kaio Murilo Gato do Amaral (Aluno) - Exercicio 1 - Introducao a computacao.
+
+def mediaFinal(mee,npf): 
+      return (2*mee + npf)/3
